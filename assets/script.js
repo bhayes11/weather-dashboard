1716d318f562b0e642e514b32a0167387c2328f1
Hayes11 <bhayes81@gmail.com>
2020-10-11 18:03:16 -0400

    updated version

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
   console.log("hello");
+
+  //Main city forecast
+  var apiKey = "dddf68f4c5f61b00d927351a5e055c6d";
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?id=" + apiKey;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(queryURL);
+    console.log(response);
+  });
 });
