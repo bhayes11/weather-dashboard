fbcc06adb02a66850c6002c8bead59fbc8bb8c97
Hayes11 <bhayes81@gmail.com>
2020-10-11 18:21:38 -0400

    updating this version

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   //Main city forecast
   var apiKey = "dddf68f4c5f61b00d927351a5e055c6d";
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?id=" + apiKey;
+
   $.ajax({
     url: queryURL,
     method: "GET",
