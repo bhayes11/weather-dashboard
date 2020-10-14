2ba45b7038304cfb8efcf2cd9328047aea8c4373
Hayes11 <bhayes81@gmail.com>
2020-10-14 00:06:26 -0400

    fixed broken html

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
-  console.log("hello");
+  //console.log("hello");
 
   //Main city forecast
+  //function cityForecast(city) {
   var apiKey = "63de61e390b4a0f5e75ff9df058d248b";
   var queryURL =
-    "https://api.openweathermap.org/data/2.5/weather?q=atlanta" +
+    "https://api.openweathermap.org/data/2.5/weather?q=Atlanta" +
+    city +
     "&appid=" +
     apiKey;
 
@@ -14,5 +16,16 @@ $(document).ready(function () {
   }).then(function (response) {
     console.log(queryURL);
     console.log(response);
+
+    //var tempF = (response.main.temp - 273.15) * 1.8 + 32;
+
+    $("currentCityTemp").text(tempF + "F");
+    //var lat = response.coord.lat;
+    //var lon = response.coord.lon;
+    //queryURL = "https://api.openweathermap.org/data/2.5/weather?q=;
   });
+  //}
+
+  //document.getElementById("searchBtn").addEventListener("click", cityForecast);
+  //console.log(searchBtn + "clicked");
 });
